2d12662d3bb1e0ded78aff1e21694ba21102dd77
stenio sousa <81321894+steniosousa@users.noreply.github.com>
2021-11-08 00:56:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ $('input').on('change', function() {
     $('body').toggleClass('blue');
   });
 
-  var texto = document.querySelector(".TextoJavaScript").innerHTML;
+  var texto = "Ajude-nos a levar esperança às vítimas das guerras.";
   var Identado = texto ;
   var result;
   
   var count = 0;
   function digitar() {
     result = document.getElementById("result");
-    window.setTimeout(function() { inserir(Identado[count]) }, 00);
+    window.setTimeout(function() { inserir(Identado[count]) }, 80);
   }
   function inserir(letra) {
     result.innerHTML += letra;
@@ -117,5 +117,50 @@ function funcoes(){
   condicionais.style.display="none";
   laços.style.display="none";
   funcoes.style.display="";
+
+}
+
+// function myFunction() {
+//   var input, filter, ul, li, a, i, txtValue;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   ul = document.getElementById("myUL");
+//   li = ul.getElementsByTagName("li");
+//   for (i = 0; i < li.length; i++) {
+//       a = li[i].getElementsByTagName("a")[0];
+//       txtValue = a.textContent || a.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//           li[i].style.display = "";
+//       } else {
+//           li[i].style.display = "none";
+//       }
+//   }
+// }
+function doar(){
+  let doar = document.querySelector(".desafios");
+  let limpar = document.querySelector(".container");
+  let limpar2 = document.querySelector(".documentação");
+  let limpar3 = document.querySelector(".conteúdo");
+  let limpar4 = document.querySelector(".lista");
+  limpar.style.display="none";
+  limpar2.style.display="none";
+  limpar3.style.display="none";
+  limpar4.style.display="none";
+
+  doar.style.display="";
+
+}
+function sala(){
+  let doar = document.querySelector(".desafios");
+  let limpar = document.querySelector(".container");
+  let limpar2 = document.querySelector(".documentação");
+  let limpar3 = document.querySelector(".conteúdo");
+  let limpar4 = document.querySelector(".lista");
+  limpar.style.display="";
+  limpar2.style.display="";
+  limpar3.style.display="";
+  limpar4.style.display="";
+
+  doar.style.display="none";
 
 }
