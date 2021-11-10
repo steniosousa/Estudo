2bee94e16fb83e615c06a9e2b93be33690a06038
stenio sousa <81321894+steniosousa@users.noreply.github.com>
2021-11-10 19:00:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,124 +1,40 @@
-$('input').on('change', function() {
-    $('body').toggleClass('blue');
-  });
+// $('input').on('change', function() {
+//     $('body').toggleClass('blue');
+//   });
 
-  var texto = "Ajude-nos a levar esperança às vítimas das guerras.";
-  var Identado = texto ;
-  var result;
+//   var texto = document.querySelector("#teste").innerHTML;
+//   var Identado = texto ;
+//   var result;
   
-  var count = 0;
-  function digitar() {
-    result = document.getElementById("result");
-    window.setTimeout(function() { inserir(Identado[count]) }, 80);
-  }
-  function inserir(letra) {
-    result.innerHTML += letra;
-    count++;
-    if(count < Identado.length)
-      window.setTimeout(function() { inserir(Identado[count]) }, 80);
-  }
-  window.onload = digitar; 
+//   var count = 0;
+//   function digitar() {
+//     result = document.getElementById("result");
+//     window.setTimeout(function() { inserir(Identado[count]) }, 80);
+//   }
+//   function inserir(letra) {
+//     result.innerHTML += letra;
+//     count++;
+//     if(count < Identado.length)
+//       window.setTimeout(function() { inserir(Identado[count]) }, 80);
+//   }
+//   window.onload = digitar; 
   
-  function scrollToIdOnClick(event){
-    event.preventDefault();
-    var element = event.target;
-    var id = element.getAttribute('href');
-    var to = document.querySelector(id).offsetTop;
-    window.scroll({
-    top: to,
-    behavior:"smooth"
-    });
-    }
-    var menuItens = document.querySelectorAll(' p');
-menuItens.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick);
-})
+//   function scrollToIdOnClick(event){
+//     event.preventDefault();
+//     var element = event.target;
+//     var id = element.getAttribute('href');
+//     var to = document.querySelector(id).offsetTop;
+//     window.scroll({
+//     top: to,
+//     behavior:"smooth"
+//     });
+//     }
+//     var menuItens = document.querySelectorAll(' p');
+// menuItens.forEach(item => {
+//     item.addEventListener('click', scrollToIdOnClick);
+// })
 
-function introdução(){
-  let introdução = document.querySelector(".abrirIntrodução");
-  let about = document.querySelector(".abrirAbout");
-  let sintaxe = document.querySelector(".abrirSintaxe");
-  let condicionais = document.querySelector(".abrirCondicionais");
-  let laços = document.querySelector(".abrirLaços");
-  let funcoes = document.querySelector(".abrirfuncoes");
-  introdução.style.display="";
-  about.style.display="none";
-  sintaxe.style.display="none";
-  condicionais.style.display="none";
-  laços.style.display="none";
-  funcoes.style.display="none";
-}
-function oQue(){
-  let introdução = document.querySelector(".abrirIntrodução");
-  let about = document.querySelector(".abrirAbout");
-  let sintaxe = document.querySelector(".abrirSintaxe");
-  let condicionais = document.querySelector(".abrirCondicionais");
-  let laços = document.querySelector(".abrirLaços");
-  let funcoes = document.querySelector(".abrirfuncoes");
-  introdução.style.display="none";
-  about.style.display="";
-  sintaxe.style.display="none";
-  condicionais.style.display="none";
-  laços.style.display="none";
-  funcoes.style.display="none";
-}
-function sintaxe(){
-  let introdução = document.querySelector(".abrirIntrodução");
-  let about = document.querySelector(".abrirAbout");
-  let sintaxe = document.querySelector(".abrirSintaxe");
-  let condicionais = document.querySelector(".abrirCondicionais");
-  let laços = document.querySelector(".abrirLaços");
-  let funcoes = document.querySelector(".abrirfuncoes");
-  introdução.style.display="none";
-  about.style.display="none";
-  sintaxe.style.display="";
-  condicionais.style.display="none";
-  laços.style.display="none";
-  funcoes.style.display="none";
-}
-function condicionais(){
-  let introdução = document.querySelector(".abrirIntrodução");
-  let about = document.querySelector(".abrirAbout");
-  let sintaxe = document.querySelector(".abrirSintaxe");
-  let condicionais = document.querySelector(".abrirCondicionais");
-  let laços = document.querySelector(".abrirLaços");
-  let funcoes = document.querySelector(".abrirfuncoes");
-  introdução.style.display="none";
-  about.style.display="none";
-  sintaxe.style.display="none";
-  condicionais.style.display="";
-  laços.style.display="none";
-  funcoes.style.display="none";
-}
-function laços(){
-  let introdução = document.querySelector(".abrirIntrodução");
-  let about = document.querySelector(".abrirAbout");
-  let sintaxe = document.querySelector(".abrirSintaxe");
-  let condicionais = document.querySelector(".abrirCondicionais");
-  let laços = document.querySelector(".abrirLaços");
-  let funcoes = document.querySelector(".abrirfuncoes");
-  introdução.style.display="none";
-  about.style.display="none";
-  sintaxe.style.display="none";
-  condicionais.style.display="none";
-  laços.style.display="";
-  funcoes.style.display="none";
-}
-function funcoes(){
-  let introdução = document.querySelector(".abrirIntrodução");
-  let about = document.querySelector(".abrirAbout");
-  let sintaxe = document.querySelector(".abrirSintaxe");
-  let condicionais = document.querySelector(".abrirCondicionais");
-  let laços = document.querySelector(".abrirLaços");
-  let funcoes = document.querySelector(".abrirfuncoes");
-  introdução.style.display="none";
-  about.style.display="none";
-  sintaxe.style.display="none";
-  condicionais.style.display="none";
-  laços.style.display="none";
-  funcoes.style.display="";
 
-}
 
 // function myFunction() {
 //   var input, filter, ul, li, a, i, txtValue;
@@ -168,3 +84,79 @@ function sala(){
 function desafios() {
   alert("Indiponíveis no momento");
 }
+function ler(){
+  let ler = document.querySelector("#ler");
+  ler.style.display="inline";
+
+  let iniciar = document.querySelector(".iniciar");
+  iniciar.addEventListener("click", function
+  (e){
+    let ler = document.querySelector("#ler");
+  ler.style.display="none";
+
+  })
+}
+
+  
+
+function Mudarestado1(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none")
+      document.getElementById(el).style.display = 'block';
+  else
+      document.getElementById(el).style.display = 'none';
+}
+
+function Mudarestado2(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none")
+      document.getElementById(el).style.display = 'block';
+  else
+      document.getElementById(el).style.display = 'none';
+}
+
+function Mudarestado3(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none")
+      document.getElementById(el).style.display = 'block';
+  else
+      document.getElementById(el).style.display = 'none';
+}
+
+function Mudarestado4(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none")
+      document.getElementById(el).style.display = 'block';
+  else
+      document.getElementById(el).style.display = 'none';
+}
+
+function Mudarestado5(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none")
+      document.getElementById(el).style.display = 'block';
+  else
+      document.getElementById(el).style.display = 'none';
+}
+
+function Mudarestado6(el) {
+  var display = document.getElementById(el).style.display;
+  if(display == "none")
+      document.getElementById(el).style.display = 'block';
+  else
+      document.getElementById(el).style.display = 'none';
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
